f56f0596d81e745af35b819f3835511e9f3b5957
Chris Dailey <nitz@users.noreply.github.com>
2021-10-08 14:06:46 -0400

    Restructured target frameworks. Windows specific...
    ... projects now build just a tiny stub. This lets the meta package get a binary (no matter how 'useless') for unsupported platforms.
    
    Also moved <NeutralLanguage /> to BuildSettings.props.

---
+++ Shared/Stub.cs
@@ -0,0 +1,12 @@
+namespace cmdwtf.Toolkit
+{
+	/// <summary>
+	/// A has-nothing do-nothing class.
+	/// This exists so that libraries that don't support
+	/// a platform can still produce an assembly, albeit blank.
+	/// </summary>
+	public static class Stub
+	{
+
+	}
+}
