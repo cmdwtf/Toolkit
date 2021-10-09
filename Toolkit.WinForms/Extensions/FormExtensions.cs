2a258a2777a9c4a63340b7a725953fbe1fb92a70
Chris Dailey <nitz@users.noreply.github.com>
2021-10-09 18:43:06 -0400

    Remove now unused extension for setting specific class longs.

--- Toolkit.WinForms/Extensions/FormExtensions.cs
+++ Toolkit.WinForms/Extensions/FormExtensions.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -82,36 +81,6 @@ namespace cmdwtf.Toolkit.WinForms.Extensions
 		{
 			var nativeRect = new RECT(rect);
 			return Native.User32.ValidateRect(window.Handle, ref nativeRect);
-		}
-
-		/// <summary>
-		/// Replaces the specified 32-bit (long) value at the specified offset into the extra class
-		/// memory or the WNDCLASSEX structure for the class to which the specified window belongs.
-		/// </summary>
-		/// <param name="hWnd">The handle to the window of the background to set.</param>
-		/// <param name="nIndex">The value to be replaced.</param>
-		/// <param name="dwNewLong">The replacement value.</param>
-		/// <returns>
-		/// If the function succeeds, the return value is the previous value of the specified 32-bit integer.
-		/// If the value was not previously set, the return value is zero.
-		/// If the function fails, the return value is zero.To get extended error information,
-		/// call GetLastError.
-		/// </returns>
-		/// <seealso>https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setclasslonga</seealso>
-		public static UIntPtr SetClassLong(this IntPtr hWnd, int nIndex, IntPtr dwNewLong)
-		{
-			unchecked
-			{
-				//check for x64
-				if (IntPtr.Size > 4)
-				{
-					return SetClassLongPtr64(hWnd, nIndex, dwNewLong);
-				}
-				else
-				{
-					return (UIntPtr)SetClassLongPtr32(hWnd, nIndex, (uint)dwNewLong.ToInt32());
-				}
-			}
 		}
 
 		/// <summary>
