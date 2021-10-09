d750d4478ee3aa10d3e81c688a783ff94560c680
Chris Dailey <nitz@users.noreply.github.com>
2021-10-09 18:45:12 -0400

    Improve ProgressBar extensions:
    Add native settings for vertical, smooth, and smoothreverse.
    Improved checking for handle creation before trying to set styles.

--- Toolkit.WinForms/Extensions/ProgressBarExtensions.cs
+++ Toolkit.WinForms/Extensions/ProgressBarExtensions.cs
@@ -1,50 +1,33 @@
 
 using System;
+using System.Windows.Forms;
+
+using cmdwtf.Toolkit.WinForms.Controls;
 
 using static cmdwtf.Toolkit.WinForms.Native.ComCtl32.ProgressBar;
-
-using SWFProgressBar = System.Windows.Forms.ProgressBar;
 
 namespace cmdwtf.Toolkit.WinForms.Extensions
 {
 	/// <summary>
-	/// Extension methods for <see cref="System.Windows.Forms.ProgressBar"/>
+	/// Extension methods for <see cref="ProgressBar"/>
 	/// </summary>
-	public static class ProgressBarExtensions
+	public static partial class ProgressBarExtensions
 
 	{
-		/// <summary>
-		/// Progress bar states.
-		/// </summary>
-		public enum State : uint
-		{
-			/// <summary>
-			/// An unknown progress bar state.
-			/// </summary>
-			Unknown = 0,
-			/// <summary>
-			/// The normal progress bar state.
-			/// </summary>
-			Normal = PBST.NORMAL,
-			/// <summary>
-			/// The error progress bar state.
-			/// </summary>
-			Error = PBST.ERROR,
-			/// <summary>
-			/// The paused progress bar state.
-			/// </summary>
-			Paused = PBST.PAUSED,
-		}
-
 		/// <summary>
 		/// Gets the current state of the progress bar.
 		/// </summary>
 		/// <param name="progressBar">The progress bar to get the state of.</param>
 		/// <returns>The current progress bar state.</returns>
-		public static State GetState(this SWFProgressBar progressBar)
+		public static ProgressBarState GetStateNative(this ProgressBar progressBar)
 		{
+			if (progressBar.IsHandleCreated == false)
+			{
+				return ProgressBarState.Unknown;
+			}
+
 			int state = Native.User32.SendMessage(progressBar.Handle, PBM.GETSTATE, UIntPtr.Zero, IntPtr.Zero);
-			return (State)state;
+			return (ProgressBarState)state;
 		}
 
 		/// <summary>
@@ -53,10 +36,74 @@ namespace cmdwtf.Toolkit.WinForms.Extensions
 		/// <param name="progressBar">The progress bar to set the state of.</param>
 		/// <param name="state">The desired state.</param>
 		/// <returns>The result of the native SendMessage call.</returns>
-		public static int SetState(this SWFProgressBar progressBar, State state)
+		public static int SetStateNative(this ProgressBar progressBar, ProgressBarState state)
 		{
+			if (progressBar.IsHandleCreated == false)
+			{
+				return -1;
+			}
+
+			if (state == ProgressBarState.Unknown ||
+				Enum.IsDefined(typeof(ProgressBarState), state) == false)
+			{
+				throw new ArgumentException($"{nameof(state)} must be a known {nameof(ProgressBarState)} value.", nameof(state));
+			}
+
 			UIntPtr wParam = new((uint)state);
 			return Native.User32.SendMessage(progressBar.Handle, PBM.SETSTATE, wParam, IntPtr.Zero);
 		}
+
+		/// <summary>
+		/// Gets the progress bar's current orientation state.
+		/// </summary>
+		/// <param name="progressBar">The progress bar to get the orientation of.</param>
+		/// <returns>The orientation of the progress bar.</returns>
+		public static Orientation GetOrientationNative(this ProgressBar progressBar)
+			=> progressBar.GetNativeStyleFlagSet(PBS.VERTICAL)
+				? Orientation.Vertical
+				: Orientation.Horizontal;
+
+		/// <summary>
+		/// Sets the progress bar's orientation.
+		/// </summary>
+		/// <param name="progressBar">The progress bar to adjust.</param>
+		/// <param name="orientation">The orientation to apply.</param>
+		/// <returns>The result of the set window long call, the previous window style.</returns>
+		public static int SetOrientationNative(this ProgressBar progressBar, Orientation orientation)
+			=> progressBar.SetNativeStyleFlag(PBS.VERTICAL, orientation == Orientation.Vertical);
+
+		/// <summary>
+		/// Gets a value indicating if the native style flag for smooth is set.
+		/// </summary>
+		/// <param name="progressBar">The window to get the style of.</param>
+		/// <returns><c>true</c>, if the style is set, otherwise <c>false</c>.</returns>
+		public static bool GetSmoothNative(this ProgressBar progressBar)
+			=> progressBar.GetNativeStyleFlagSet(PBS.SMOOTH);
+
+		/// <summary>
+		/// Sets the native style for smooth to be enabled or disabled.
+		/// </summary>
+		/// <param name="progressBar">The window to set the style on.</param>
+		/// <param name="smooth">The state of the style to set.</param>
+		/// <returns>The previous window style value.</returns>
+		public static int SetSmoothNative(this ProgressBar progressBar, bool smooth)
+			=> progressBar.SetNativeStyleFlag(PBS.SMOOTH, smooth);
+
+		/// <summary>
+		/// Gets a value indicating if the native style flag for smooth reverse is set.
+		/// </summary>
+		/// <param name="progressBar">The window to get the style of.</param>
+		/// <returns><c>true</c>, if the style is set, otherwise <c>false</c>.</returns>
+		public static bool GetSmoothReverseNative(this ProgressBar progressBar)
+			=> progressBar.GetNativeStyleFlagSet(PBS.SMOOTHREVERSE);
+
+		/// <summary>
+		/// Sets the native style for smooth reverse to be enabled or disabled.
+		/// </summary>
+		/// <param name="progressBar">The window to set the style on.</param>
+		/// <param name="smooth">The state of the style to set.</param>
+		/// <returns>The previous window style value.</returns>
+		public static int SetSmoothReverseNative(this ProgressBar progressBar, bool smooth)
+			=> progressBar.SetNativeStyleFlag(PBS.SMOOTHREVERSE, smooth);
 	}
 }
--- Toolkit.WinForms/Native/ProgressBar.cs
+++ Toolkit.WinForms/Native/ProgressBar.cs
@@ -19,22 +19,22 @@ namespace cmdwtf.Toolkit.WinForms.Native
 			/// </summary>
 			public static class PBM
 			{
-				public const uint SETRANGE = WM_USER + 1;
-				public const uint SETPOS = WM_USER + 2;
-				public const uint DELTAPOS = WM_USER + 3;
-				public const uint SETSTEP = WM_USER + 4;
-				public const uint STEPIT = WM_USER + 5;
-				public const uint SETRANGE32 = WM_USER + 6;
-				public const uint GETRANGE = WM_USER + 7;
-				public const uint GETPOS = WM_USER + 8;
-				public const uint SETBARCOLOR = WM_USER + 9;
-				public const uint SETBKCOLOR = CCM.SETBKCOLOR;
-				public const uint SETMARQUEE = WM_USER + 10;
-				public const uint GETSTEP = WM_USER + 13;
-				public const uint GETBKCOLOR = WM_USER + 14;
-				public const uint GETBARCOLOR = WM_USER + 15;
-				public const uint SETSTATE = WM_USER + 16; // wParam = PBST.[state]
-				public const uint GETSTATE = WM_USER + 17;
+				public const int SETRANGE = WM_USER + 1;
+				public const int SETPOS = WM_USER + 2;
+				public const int DELTAPOS = WM_USER + 3;
+				public const int SETSTEP = WM_USER + 4;
+				public const int STEPIT = WM_USER + 5;
+				public const int SETRANGE32 = WM_USER + 6;
+				public const int GETRANGE = WM_USER + 7;
+				public const int GETPOS = WM_USER + 8;
+				public const int SETBARCOLOR = WM_USER + 9;
+				public const int SETBKCOLOR = CCM.SETBKCOLOR;
+				public const int SETMARQUEE = WM_USER + 10;
+				public const int GETSTEP = WM_USER + 13;
+				public const int GETBKCOLOR = WM_USER + 14;
+				public const int GETBARCOLOR = WM_USER + 15;
+				public const int SETSTATE = WM_USER + 16; // wParam = PBST.[state]
+				public const int GETSTATE = WM_USER + 17;
 			}
 
 			/// <summary>
@@ -42,10 +42,10 @@ namespace cmdwtf.Toolkit.WinForms.Native
 			/// </summary>
 			public static class PBS
 			{
-				public const uint SMOOTH = 0x01;
-				public const uint VERTICAL = 0x04;
-				public const uint MARQUEE = 0x08;
-				public const uint SMOOTHREVERSE = 0x10;
+				public const int SMOOTH = 0x01;
+				public const int VERTICAL = 0x04;
+				public const int MARQUEE = 0x08;
+				public const int SMOOTHREVERSE = 0x10;
 			}
 
 			/// <summary>
@@ -53,9 +53,9 @@ namespace cmdwtf.Toolkit.WinForms.Native
 			/// </summary>
 			public static class PBST
 			{
-				public const uint NORMAL = 0x0001;
-				public const uint ERROR = 0x0002;
-				public const uint PAUSED = 0x0003;
+				public const int NORMAL = 0x0001;
+				public const int ERROR = 0x0002;
+				public const int PAUSED = 0x0003;
 			}
 
 			#region Visual Styles
